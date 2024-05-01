25a78f805ca137918a2a3a6d3ca565ed10eae5a0
Henryearls <henryearls@icloud.com>
2024-05-01 09:49:40 -0400

    harris

--- project 3/time.js
+++ project 3/time.js
@@ -1,35 +1,25 @@
-// Function to update the particles configuration
 function updateParticles() {
     let today = new Date();
     let thisSecond = today.getSeconds();
 
-    // Map the number of seconds to a range of particles from 1 to 400
-    let mappedParticles = map(thisSecond, 0, 59, 1, 400);
+    // Map the number of seconds to a hue value between 0 and 360
+    let mappedHue = map(thisSecond, 0, 59, 0, 360);
 
-    // Modify the number of particles based on the mapped value
-    let particlesJSON = {
-        "particles": {
-            "number": {
-                "value": mappedParticles // Set the number of particles to the mapped value
-            },
-            // Other particle configurations...
-        },
-        "interactivity": {
-            // Interactivity configurations...
-        }
-    };
+    // Set the background color of the particles-js div
+    let particlesDiv = document.getElementById('particles-js');
+    particlesDiv.style.background = "hsl(" + mappedHue + ", 100%, 50%)";
 
-    // Use the modified configuration
-    particlesJS('particles-js', particlesJSON);
+    // Set the background color of the overlay div
+    let overlayDiv = document.getElementById('overlay');
+    overlayDiv.style.backgroundColor = "hsla(" + mappedHue + ", 100%, 50%, 0.5)"; // 50% transparent
+
+    // Change the size of the div based on the current amount of seconds
+    let element = document.getElementById("myDiv");
+    let mappedWidth = map(thisSecond, 0, 59, 0, 200); // Map seconds to width
+    let mappedHeight = map(thisSecond, 0, 59, 0, 100); // Map seconds to height
+    element.style.width = mappedWidth + "px";
+    element.style.height = mappedHeight + "px";
 }
-
-// Call the updateParticles function initially
-updateParticles();
 
 // Call the updateParticles function every second
-setInterval(updateParticles, 1000);
-
-// DO NOT TOUCH THIS!
-function map(value, low1, high1, low2, high2) {
-    return low2 + (high2 - low2) * (value - low1) / (high1 - low1);
-}
+setInterval(updateParticles, 100);
